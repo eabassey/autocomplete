09d6c35c412850de9b0fd13612de41b188652dad
Enoch Abassey <eabassey@yahoo.com>
2018-10-17 20:48:18 +0200

    good base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { SelectItem } from './utils/selectitem';
+
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'autocomplete';
+  cities: City[];
+
+  selectedCity: City;
+
+  cars: SelectItem[];
+
+  selectedCar1: string;
+
+  selectedCar2: string = 'BMW';
+
+  selectedCar3: string;
+
+  groupedCars: any[];
+
+  constructor() {
+      this.cities = [
+          {name: 'New York', code: 'NY'},
+          {name: 'Rome', code: 'RM'},
+          {name: 'London', code: 'LDN'},
+          {name: 'Istanbul', code: 'IST'},
+          {name: 'Paris', code: 'PRS'}
+      ];
+
+      this.cars = [
+          {label: 'Audi', value: 'Audi'},
+          {label: 'BMW', value: 'BMW'},
+          {label: 'Fiat', value: 'Fiat'},
+          {label: 'Ford', value: 'Ford'},
+          {label: 'Honda', value: 'Honda'},
+          {label: 'Jaguar', value: 'Jaguar'},
+          {label: 'Mercedes', value: 'Mercedes'},
+          {label: 'Renault', value: 'Renault'},
+          {label: 'VW', value: 'VW'},
+          {label: 'Volvo', value: 'Volvo'}
+      ];
+
+      this.groupedCars = [
+          {
+              label: 'Germany', value: 'germany.png',
+              items: [
+                  {label: 'Audi', value: 'Audi'},
+                  {label: 'BMW', value: 'BMW'},
+                  {label: 'Mercedes', value: 'Mercedes'}
+              ]
+          },
+          {
+              label: 'USA', value: 'usa.png',
+              items: [
+                  {label: 'Cadillac', value: 'Cadillac'},
+                  {label: 'Ford', value: 'Ford'},
+                  {label: 'GMC', value: 'GMC'}
+              ]
+          },
+          {
+              label: 'Japan', value: 'japan.png',
+              items: [
+                  {label: 'Honda', value: 'Honda'},
+                  {label: 'Mazda', value: 'Mazda'},
+                  {label: 'Toyota', value: 'Toyota'}
+              ]
+          }
+      ];
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DropdownModule } from './dropdown/dropdown.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
